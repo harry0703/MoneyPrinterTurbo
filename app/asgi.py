ad568ac0ccdbac49d329756ace1be1f997e8d6b3
chenxin.chenxin <chenxin.chenxin@alibaba-inc.com>
2024-03-29 17:39:12 +0800

    add:videos

--- app/asgi.py
+++ app/asgi.py
@@ -44,7 +44,7 @@ def get_application() -> FastAPI:
     # instance.mount("/", StaticFiles(directory=utils.public_dir(), html=True), name="")
 
     instance.include_router(root_api_router)
-    # instance.add_exception_handler(HttpException, exception_handler)
+    instance.add_exception_handler(HttpException, exception_handler)
     instance.add_exception_handler(RequestValidationError, validation_exception_handler)
     return instance
 
--- app/controllers/video.py
+++ app/controllers/video.py
@@ -1,10 +1,14 @@
 from fastapi import APIRouter
 from fastapi import Request
 from fastapi.responses import FileResponse, Response
-router = APIRouter()
 
-@route.get("/videos/{rest_of_path:path}", response_class=Response, include_in_schema=False)
-async def static_files(
-    rest_of_path: str,
-):
-    return FileResponse(f"/videos/{rest_of_path}")
+router = APIRouter()
+# @route.get("/videos/{rest_of_path:path}", response_class=Response, include_in_schema=False)
+# async def static_files(
+#     rest_of_path: str,
+# ):
+#     return FileResponse(f"/videos/{rest_of_path}")
+
+@router.get("/videos/{task_id}", response_class=Response)
+def get_task(request: Request, task_id: str):
+    return task_id
