d711fde73f75c2ed9433d03beb4d0135a3a12691
harry <harry@wangxutech.com>
2024-04-03 14:11:15 +0800

    optimize logs

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from loguru import logger
 from app.config import config
 
 if __name__ == '__main__':
-    logger.info("start server, docs: http://127.0.0.1:" + str(config.listen_port) + "/docs")
+    logger.info("start server, docs: http://0.0.0.0:" + str(config.listen_port) + "/docs")
     uvicorn.run(app="app.asgi:app", host=config.listen_host, port=config.listen_port, reload=config.reload_debug,
                 log_level="warning")
