e6c5dd4146e88e0c3319b3b57331e7bb0b9c6e01
harry <harry@wangxutech.com>
2024-04-05 22:43:20 +0800

    update the startup script

--- webui.bat
+++ webui.bat
@@ -2,4 +2,12 @@
 set CURRENT_DIR=%CD%
 echo ***** Current directory: %CURRENT_DIR% *****
 set PYTHONPATH=%CURRENT_DIR%;%PYTHONPATH%
+
+rem If you could not download the model from the official site, you can use the mirror site.
+rem Just remove the comment of the following line (remove rem).
+rem 如果你无法从官方网站下载模型，你可以使用镜像网站。
+rem 只需要移除下面一行的注释即可（移除 rem）。
+
+rem set HF_ENDPOINT=https://hf-mirror.com
+
 streamlit run .\webui\Main.py
--- webui.sh
+++ webui.sh
@@ -1,4 +1,12 @@
 CURRENT_DIR=$(pwd)
 echo "***** Current directory: $CURRENT_DIR *****"
 export PYTHONPATH="${CURRENT_DIR}:$PYTHONPATH"
-streamlit run ./webui/Main.py
+
+# If you could not download the model from the official site, you can use the mirror site.
+# Just remove the comment of the following line .
+# 如果你无法从官方网站下载模型，你可以使用镜像网站。
+# 只需要移除下面一行的注释即可。
+
+# export HF_ENDPOINT=https://hf-mirror.com
+
+streamlit run ./webui/Main.py --browser.serverAddress="0.0.0.0" --server.enableCORS=True --browser.gatherUsageStats=False
