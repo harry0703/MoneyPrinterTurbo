6de3d6eedce0d3e5c4f0a7f00f38d9a1d4b9b8f8
harry <harry@wangxutech.com>
2024-05-13 18:29:59 +0800

    1. support voice preview 2, update version to 1.1.6

--- app/services/voice.py
+++ app/services/voice.py
@@ -1035,6 +1035,7 @@ def tts(text: str, voice_name: str, voice_file: str) -> [SubMaker, None]:
 
 
 def azure_tts_v1(text: str, voice_name: str, voice_file: str) -> [SubMaker, None]:
+    voice_name = parse_voice_name(voice_name)
     text = text.strip()
     for i in range(3):
         try:
