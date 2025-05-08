5177c1871a75a2e6b3be48825d000e9c340aee4f
harry <harry@wangxutech.com>
2025-05-08 17:34:09 +0800

    feat: comment out interline and size parameters in video.py

--- app/services/video.py
+++ app/services/video.py
@@ -289,8 +289,8 @@ def generate_video(
             bg_color=params.text_background_color,
             stroke_color=params.stroke_color,
             stroke_width=params.stroke_width,
-            interline=interline,
-            size=size,
+            # interline=interline,
+            # size=size,
         )
         duration = subtitle_item[0][1] - subtitle_item[0][0]
         _clip = _clip.with_start(subtitle_item[0][0])
