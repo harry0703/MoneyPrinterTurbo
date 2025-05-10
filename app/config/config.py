3108c2e4e5e12cbe54e470be52978d4e6ac4a154
harry <harry@wangxutech.com>
2025-05-10 14:13:18 +0800

    perf: bump version to 1.2.6

--- app/config/config.py
+++ app/config/config.py
@@ -64,7 +64,7 @@ project_description = _cfg.get(
     "project_description",
     "<a href='https://github.com/harry0703/MoneyPrinterTurbo'>https://github.com/harry0703/MoneyPrinterTurbo</a>",
 )
-project_version = _cfg.get("project_version", "1.2.5")
+project_version = _cfg.get("project_version", "1.2.6")
 reload_debug = False
 
 imagemagick_path = app.get("imagemagick_path", "")
