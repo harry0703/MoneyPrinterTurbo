4fcedbcc2e63cbdd90eba344f54a3d0122ee2daf
RyanX <looe_xiao@hotmail.com>
2026-05-16 02:22:10 +0800

    chore: remove all streamlit webui related code and dependencies
    This commit completely removes the Streamlit web UI implementation from the codebase, including:
    1. Deleted all webui component files (ui_task_components.py, ui_script_components.py, etc.)
    2. Removed Streamlit configuration files and startup scripts
    3. Removed all i18n translation files for non-English languages
    4. Removed streamlit dependency from requirements.txt
    5. Deleted the bundled demo image

--- webui.sh
+++
@@ -1,8 +0,0 @@
-# If you could not download the model from the official site, you can use the mirror site.
-# Just remove the comment of the following line .
-# 如果你无法从官方网站下载模型，你可以使用镜像网站。
-# 只需要移除下面一行的注释即可。
-
-# export HF_ENDPOINT=https://hf-mirror.com
-
-streamlit run ./webui/Main.py --browser.serverAddress="0.0.0.0" --server.enableCORS=True --browser.gatherUsageStats=False
