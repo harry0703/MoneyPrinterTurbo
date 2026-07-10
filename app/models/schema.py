3079511026a78b7a32ff5b0f6b156ed3f6287f93
Paulo C. Benetti <pcbntt@gmail.com>
2026-07-09 21:37:30 -0300

    feat(schema): add video_clip_speed param (default 1.0)

--- app/models/schema.py
+++ app/models/schema.py
@@ -77,6 +77,7 @@ class VideoParams(BaseModel):
     video_concat_mode: Optional[VideoConcatMode] = VideoConcatMode.random.value
     video_transition_mode: Optional[VideoTransitionMode] = None
     video_clip_duration: Optional[int] = 5
+    video_clip_speed: Optional[float] = 1.0
     match_materials_to_script: bool = False
     video_count: Optional[int] = 1
 
